ee93ff92b8079d86947c2bb24bf3d30d0fcdd5a8
sildvauresberg <slickunit4265752@gmail.com>
2023-02-28 01:24:45 +0100

    [zsh] [k8s] [vim] [docker] minor changes
    remove custom completions (unused)
    
    change zsh syntax highlighting plugin
    
    add custom k8s functions

--- roles/k8s/files/k8s.sh
+++ roles/k8s/files/k8s.sh
@@ -16,3 +16,15 @@ alias kgn="kubectl get nodes"
 
 # shellcheck source=/dev/null
 command -v kubectl &>/dev/null && source <(kubectl completion zsh)
+
+list_pods() {
+  for ns in $(kgns | tail -n +2 | awk '{print $1}'); do
+    echo pods in $ns namespace;
+    kgp --namespace $ns;
+    echo '\n';
+  done
+}
+
+list_resources() {
+  kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found --namespace kube-system
+}
