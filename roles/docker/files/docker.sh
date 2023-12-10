e7ee15caed518ed64c1e004ae9c1d63da1e12ffc
Kirill Osintsev <slickunit4265752@gmail.com>
2023-12-10 17:41:10 +0100

    Add cmd: delete all tags of a docker img by name

--- roles/docker/files/docker.sh
+++ roles/docker/files/docker.sh
@@ -1,5 +1,10 @@
 alias dis="docker images --format \"{{.ID}}\t{{.Size}}\t{{.Repository}}\" | sort -hk2"
 
+docker_remove_image_by_name() {
+  id=$(docker images --filter=reference="$1:*" --format "{{ .ID }}")
+  [ ! -z "$id" ] && docker rmi "$id"
+}
+
 drm() {
   docker rmi "$(docker images -f 'dangling=true' | awk '{if ($1 == "<none>") {print $3 } ')"
 }
