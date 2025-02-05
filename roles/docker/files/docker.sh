ef05a7e3a35d36aea02b63103f29d60b24cc7100
Kirill Osintsev <kosintsev.dev@gmail.com>
2025-02-05 21:40:01 +0100

    docker: add alias d='docker'

--- roles/docker/files/docker.sh
+++ roles/docker/files/docker.sh
@@ -2,12 +2,8 @@
 
 alias vmgo='colima start --mount $(dirname $(pwd)):w'
 
+alias d='docker'
 alias dis="docker images --format \"{{.ID}}\t{{.Size}}\t{{.Repository}}\" | sort -hk2"
-
-docker-rmi-by-name() {
-  id=$(docker images --filter=reference="$1:*" --format "{{ .ID }}")
-  [ -n "$id" ] && docker rmi "$id"
-}
 
 alias docker-rmi-force='docker rmi -f $(docker images -a -q)'
 alias docker-rmc-force='docker ps -q | xargs docker rm -f'
@@ -16,6 +12,11 @@ alias docker-rmc="docker ps -a --filter=status=exited --format='{{ .ID }}' | xar
 drm() {
   docker rmi "$(docker images -f 'dangling=true' \
     | awk '$1 == "<none>" { print $3 }' ORS=' ')"
+}
+
+docker-rmi-by-name() {
+  id=$(docker images --filter=reference="$1:*" --format "{{ .ID }}")
+  [ -n "$id" ] && docker rmi "$id"
 }
 
 docker-clean() {
